3d2d4556a8afea9b75aaf520de08606678ce50d8
DanyloRudavets <danilorudavec1@gmail.com>
2024-10-11 12:20:22 +0200

    3 section- completed

---
+++ 02-TypesAndVariables/sec3_2.py
@@ -0,0 +1,12 @@
+###
+# A program that calculates the sum of two numbers.
+# Modify the program to calculate the sum of three numbers.
+#
+number1 = 71
+number2 = 14
+n3=int(input('d:'))
+result = number1 + number2 + n3
+print('Number 1: ', number1)
+print('Number 2: ', number2)
+print('number 3', n3)
+print('The result of summation: ', result)
---
+++ 02-TypesAndVariables/sec3_6.py
@@ -0,0 +1,6 @@
+import math
+
+h=int(input('h:'))
+d = 3.57 * math.sqrt(h)
+print('the distance is:',d,'km')
+      
