5b0dce330b7d7f726a481a2600bda2f12d7bc626
DanyloRudavets <danilorudavec1@gmail.com>
2024-10-18 20:49:05 +0200

    section 6 completed

---
+++ 03-ControlStructures/sec6_18.py
@@ -0,0 +1,15 @@
+i=input('Are you interested in computer science(y/n):')
+cg=input('Do you like playing computer games? (y/n):')
+ia=input('Do you have an Instagram account? (y/n):')
+if i =='y':
+    print('Interested in computer science: Yes')
+elif i =='n':
+    print('Interested in computer science: No')
+if cg=='y':
+    print('Playing computer games: Yes')
+elif cg =='n':
+    print('Playing computer games: No')
+if ia=='y':
+    print('Has an Instagram account: Yes')
+elif ia=='n':
+    print('Has an Instagram account: No')
