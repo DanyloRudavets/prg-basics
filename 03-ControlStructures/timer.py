1f99d68fe2a0e4c73b6d1af4d8872900c20e42d7
DanyloRudavets <danilorudavec1@gmail.com>
2024-10-18 13:47:20 +0200

    timer done

--- 03-ControlStructures/timer.py
+++ 03-ControlStructures/timer.py
@@ -11,4 +11,16 @@ countdown = int(input("Enter the number of seconds to count down: "))
 while countdown > 0:
     print(countdown)
     countdown -= 1
-    time.sleep(1)  # Wait for 1 second
+    time.sleep(1) 
+    if countdown==5:
+        print('five')
+    elif countdown==4:
+        print('four')
+    elif countdown==3:
+        print('Three'
+        )
+    elif countdown==2:
+        print('two')
+    elif countdown==1:
+        print('one')
+print('Your time is up')
