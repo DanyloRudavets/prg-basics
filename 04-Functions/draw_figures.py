bd8f85bce652b473a5925be69d9ec5dd2ed0bc73
DanyloRudavets <danilorudavec1@gmail.com>
2024-11-01 22:22:51 +0100

    section 5 completed

--- 04-Functions/draw_figures.py
+++ 04-Functions/draw_figures.py
@@ -1,16 +1,47 @@
 import turtle
 import figures
 side_length=figures.sdl('entre your number')
+side_lengt=figures.tr("enter your number")
+side_leng=figures.rect('enter your number')
+side_width=figures.rectt("enter your number")
 
 
 def draw_square(side_length):
+    global window
     window = turtle.Screen()
     window.bgcolor("lightgreen")
+    global pen
     pen = turtle.Turtle()
     pen.speed(5)
     for i in range(4):
         pen.forward(side_length)
         pen.right(90)
-    pen.hideturtle()
-    window.mainloop()
+    pen.penup()
+    pen.goto(-100,100)
+    pen.down()
+def draw_triangle(side_lengt):
+    pen.speed(5)
+    for g in range(3):
+        pen.right(120)
+        pen.forward(side_lengt)
+    pen.penup()
+    pen.goto(-250,250)
+    pen.down()
+def draw_rectangle(side_leng,side_width):
+    pen.speed(5)
+    for g in range(4):
+        if g%2==1:
+            pen.right(90)
+            pen.forward(side_leng)
+        if g%2==0:
+            pen.right(90)
+            pen.forward(side_width)
+    pen.penup()
+    pen.goto(400,-100)
+    pen.down()
 draw_square(side_length)
+draw_triangle(side_lengt)
+draw_rectangle(side_leng,side_width)
+pen.hideturtle()
+
+window.mainloop()
--- 04-Functions/figures.py
+++ 04-Functions/figures.py
@@ -2,3 +2,13 @@
 def sdl(sl):
     sl=int(input(sl))
     return sl
+def tr(l):
+    l=int(input(l))
+    return l
+def rect(ll):
+    ll=int(input(ll))
+    return ll
+def rectt(g):
+    g=int(input(g))
+    return g
+
