9863bf23a87d51ddbf3b9fd2a33bdcfe17ac9524
DanyloRudavets <danilorudavec1@gmail.com>
2024-11-28 23:09:42 +0100

    section 1 completed

---
+++ 07-Arrays/sec1_16.py
@@ -0,0 +1,10 @@
+distances_traveled = [120, 150, 180, 90, 200, 175, 160]
+sort_distance_traveled=sorted(distances_traveled)
+print(sort_distance_traveled)
+daily_temperatures = [16, 17, 15, 14, 18, 19, 17, 15, 16, 18]
+sort_daily_temparatures=sorted(daily_temperatures, reverse=True)
+print(sort_daily_temparatures)
+file_names = ["report.docx", "presentation.pptx", "data.csv", "photo.jpg", "notes.txt",
+   "invoice.pdf", "resume.docx", "budget.xlsx", "meeting.mp4", "schedule.pdf"]
+sort_f=sorted(file_names)
+print(sort_f)
