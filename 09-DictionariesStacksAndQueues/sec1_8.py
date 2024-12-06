64cd6693227f4ad3df893f1bdf32a4605da28689
DanyloRudavets <danilorudavec1@gmail.com>
2024-12-06 12:29:59 +0100

    sec 2 completed

--- 09-DictionariesStacksAndQueues/sec1_8.py
+++ 09-DictionariesStacksAndQueues/sec1_8.py
@@ -5,4 +5,17 @@ price_list = {
    'Sneakers': 59.99,
    'Hat': 15.99
 }
+q=0
+w=0
 for c,p in price_list.items():
+    print(f'{c} {p}')
+for p in price_list.values():
+    q += p
+print(f'{q: .2f}')
+for c,p in price_list.items():
+    p=p*0.9
+    price_list[c]=int(p)
+print(price_list)
+for p  in price_list.values():
+    w+=p
+print(w)
