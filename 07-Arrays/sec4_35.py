e285c2277113ef30fa983a5f1a7ec86dc08e91da
DanyloRudavets <danilorudavec1@gmail.com>
2024-12-08 00:42:14 +0100

    section 4 completed

--- 07-Arrays/sec4_35.py
+++ 07-Arrays/sec4_35.py
@@ -1,9 +1,18 @@
 def transpose_matrix(m):
     try:
+        k=[]
+        c=[]
         for i in range(len(m[0])):
             for j in range(len(m)):
-                print(m[j][i], end=' ')
-            print()
+                k.append(m[j][i])
+                if len(k)==len(m):
+                    c.append(k)
+                    k=[]
+
+
+
+        print(c)
+
     except TypeError:
         print(m)
 
@@ -13,5 +22,7 @@ def transpose_matrix(m):
 transpose_matrix([[1,2,3],
    [4,5,6],
    [7,8,9]])
+transpose_matrix([[5,0,3,7,5],[9,0,9,1,2]])
 transpose_matrix([[1,2,3,4,5],[6,7,8,9,0]])
 transpose_matrix([5,6,7,8])
+
